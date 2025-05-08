907025fabb7f4321606904bd9b8bc4c95d554178
ArngrimurEsra <arngrimur24@ru.is>
2025-05-08 20:13:36 +0000

    bla

--- LuxTerra/views.py
+++ LuxTerra/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, '_navigationba.html')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about_us.html')
