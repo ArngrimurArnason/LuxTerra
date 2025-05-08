c7e5968832935ca2090061af80df93ef279b6f82
ArngrimurEsra <arngrimur24@ru.is>
2025-05-08 11:30:37 +0000

    urls

--- property/views.py
+++ property/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def properties(request):
+    pass
+def list_property(request):
+    pass
+
+def property_details(request, property_id):
+    pass
