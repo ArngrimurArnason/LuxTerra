faa7a766dca4e8ef1ee1e1a4c001cd78262f818c
arnarmani11 <arnarmani03@gmail.com>
2025-05-08 12:54:50 +0000

    asd

--- LuxTerra/site_urls.py
+++ LuxTerra/site_urls.py
@@ -1,7 +1,8 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
-
+from LuxTerra import views
 urlpatterns = [
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
+    path('', views.index, name='index')
+    #path('about/', views.about, name='about'),
+    #path('contact/', views.contact, name='contact'),
 ]
--- LuxTerra/views.py
+++ LuxTerra/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'luxterra/index.html')
+    return render(request, 'navigationba.html')
