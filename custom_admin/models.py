b3462d6edffc45241c9cdebca13e0ff0c8127d0e
ArngrimurEsra <arngrimur24@ru.is>
2025-05-12 14:43:47 +0000

    fleiri template sett inn, ásamt login byrjun

--- custom_admin/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-class Custom_Admin(models.Model):
-    adminid = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-
--- user/models.py
+++ user/models.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(models.Model):
+class User(AbstractUser):
     user_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    profile_img = models.CharField(max_length=4096)
+    profile_img = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     address = models.CharField(max_length=100, blank=True, null=True)
     logo = models.CharField(max_length=100, blank=True, null=True)
