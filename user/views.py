7a02819a35a42d334a2544c7a82505d034859075
ArngrimurEsra <arngrimur24@ru.is>
2025-05-13 08:07:06 +0000

    login html and user views upd

--- user/views.py
+++ user/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, get_user_model
+from django.shortcuts import render, redirect
+from django.contrib import messages
 # Create your views here.
 def account_info(request):
     return render(request, 'account_info.html')
@@ -12,7 +14,36 @@ def edit_profile(request):
 def my_listings(request):
     return render(request, 'my_listings.html')
 
+
+
+User = get_user_model()
+
 def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        print("EMAIL:", email)
+        print("PASSWORD:", password)
+
+        try:
+            user_obj = User.objects.get(email=email)
+            print("Found user:", user_obj)
+
+            # now try to authenticate
+            user = authenticate(request, username=user_obj.username, password=password)
+            print("Authenticated user:", user)
+
+        except User.DoesNotExist:
+            print("No user with that email.")
+            user = None
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid email or password.')
+
     return render(request, 'login.html')
 
 def signup_view(request):
