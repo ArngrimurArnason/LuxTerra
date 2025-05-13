40a39aebbd46189678341a858a5202b24b5acdc4
Arngrimur Arnason <arngrimurarnason@gmail.com>
2025-05-13 13:20:31 +0000

    fixed login error messages

--- property/models.py
+++ property/models.py
@@ -19,6 +19,12 @@ class Property(models.Model):
     property_status = models.BooleanField()
     thumbnail = models.ImageField(upload_to='property_thumbnails/', blank=True, null=True)
 
+def __str__(self):
+    return f"{self.street} {self.house_number} - {self.city}"
+
+
 class PropertyImages(models.Model):
     property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='property_images/')
+
+
