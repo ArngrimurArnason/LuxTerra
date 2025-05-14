4003956e57d2de0510adcd7141e2f253e341023d
Arngrimur Arnason <arngrimurarnason@gmail.com>
2025-05-14 13:35:55 +0000

    validation update

--- user/Forms/sign_up_form.py
+++ user/Forms/sign_up_form.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
 from user.models import User
 from django import forms
+from django.core.exceptions import ValidationError
+import re
 
 
 class SignUpForm(ModelForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        required=True
+    )
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
@@ -20,6 +26,36 @@ class SignUpForm(ModelForm):
             'profile_img': forms.ClearableFileInput(attrs={'class': 'form-control'}),
             'bio': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
+    def clean_national_id(self):
+        nid = self.cleaned_data.get('national_id')
+        if not str(nid).isdigit():
+            raise ValidationError("SSN must contain only digits.")
+        if len(str(nid)) != 10:
+            raise ValidationError("SSN must be exactly 10 digits.")
+        return nid
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if ' ' in username:
+            raise ValidationError("Username must not contain spaces.")
+        if not username.isalpha():
+            raise ValidationError("Username must contain only letters (a–z). No digits or symbols.")
+        if len(username) < 3:
+            raise ValidationError("Username must be at least 3 characters long.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email already in use.")
+        return email
+
+    def clean_address(self):
+        address = self.cleaned_data.get('address')
+        if address and len(address) > 30:
+            raise ValidationError("Address too long.")
+        return address
 
     def clean(self):
         cleaned_data = super().clean()
