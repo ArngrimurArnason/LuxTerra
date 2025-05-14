af27a83fb832ed343e16e3e0fd389b6e9fc17344
Arngrimur Arnason <arngrimurarnason@gmail.com>
2025-05-14 16:21:08 +0000

    properties upd

--- property/views.py
+++ property/views.py
@@ -15,73 +15,74 @@ from django.shortcuts import render, redirect
 from .forms import list_property_forms  # and any others if needed
 
 def properties_view(request):
-  properties = Property.objects.filter(admin_approval=True)
+    properties = Property.objects.filter(admin_approval=True)
 
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    property_type = request.GET.get('property_type')
+    bedrooms = request.GET.get('bedrooms')
+    postal_code = request.GET.get('postal_code')
+    street_name = request.GET.get('street_name')
+    order_by = request.GET.get('order_by')
 
-  city = request.GET.get('city')
-  min_price = request.GET.get('min_price')
-  max_price = request.GET.get('max_price')
-  property_type = request.GET.get('property_type')
-  bedrooms = request.GET.get('bedrooms')
-  postal_code = request.GET.get('postal_code')
-  street_name = request.GET.get('street_name')
-  order_by = request.GET.get('order_by')
+    # Sorting
+    if order_by == 'price_asc':
+        properties = properties.order_by('price')
+    elif order_by == 'price_desc':
+        properties = properties.order_by('-price')
+    elif order_by == 'size_asc':
+        properties = properties.order_by('size')
+    elif order_by == 'size_desc':
+        properties = properties.order_by('-size')
+    elif order_by == 'date_asc':
+        properties = properties.order_by('build_date')
+    elif order_by == 'date_desc':
+        properties = properties.order_by('-build_date')
 
-  if order_by == 'price_asc':
-    properties = properties.order_by('price')
-  elif order_by == 'price_desc':
-    properties = properties.order_by('-price')
-  elif order_by == 'size_asc':
-    properties = properties.order_by('size')
-  elif order_by == 'size_desc':
-    properties = properties.order_by('-size')
-  elif order_by == 'date_asc':
-    properties = properties.order_by('build_date')
-  elif order_by == 'date_desc':
-    properties = properties.order_by('-build_date')
+    # Filters
+    if postal_code:
+        properties = properties.filter(location__startswith=postal_code)
 
-  if city:
-    properties = properties.filter(city__icontains=city)
+    if property_type:
+        properties = properties.filter(property_type__iexact=property_type)
 
-  if postal_code:
-    properties = properties.filter(post_code=postal_code)
+    if min_price:
+        properties = properties.filter(price__gte=min_price)
 
-  if property_type:
-    properties = properties.filter(property_type__iexact=property_type)
+    if max_price:
+        properties = properties.filter(price__lte=max_price)
 
-  if min_price:
-    properties = properties.filter(price__gte=min_price)
+    if bedrooms:
+        properties = properties.filter(bedrooms=bedrooms)
 
-  if max_price:
-    properties = properties.filter(price__lte=max_price)
+    if street_name:
+        properties = properties.filter(
+            Q(street__icontains=street_name) |
+            Q(location__icontains=street_name)
+        )
 
-  if bedrooms:
-    properties = properties.filter(bedrooms=bedrooms)
+    # Related images
+    all_images = PropertyImages.objects.all()
 
+    # Price filter options
+    price_steps = [
+        0, 50000000, 100000000, 150000000,
+        200000000, 250000000, 300000000, 350000000, 400000000
+    ]
 
-  if street_name:
-    properties = properties.filter(
-      Q(street__icontains=street_name) |
-      Q(city__icontains=street_name)
-    )
-
-
-  all_images = PropertyImages.objects.all()
-
-  price_steps = [0, 50000000, 100000000, 150000000, 200000000, 250000000,
-                 300000000, 350000000, 400000000]
-  return render(request, 'all_properties.html', {
-    "properties": properties,
-    "property_images": all_images,
-    "filters": {
-      "city": city,
-      "min_price": min_price,
-      "max_price": max_price,
-      "property_type": property_type,
-      "bedrooms": bedrooms,
-    },
-    "prices": price_steps,
-  })
+    return render(request, 'all_properties.html', {
+        "properties": properties,
+        "property_images": all_images,
+        "filters": {
+            "min_price": min_price,
+            "max_price": max_price,
+            "property_type": property_type,
+            "bedrooms": bedrooms,
+            "postal_code": postal_code,
+            "street_name": street_name,
+        },
+        "prices": price_steps,
+    })
 
 
 
