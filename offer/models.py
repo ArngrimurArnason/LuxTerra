43ebd435c79180b062e268d9379c3667666f10fb
Siggibjarki <sigurdurj23@ru.is>
2025-05-15 16:07:34 +0000

    offer functionality

--- offer/models.py
+++ offer/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from user.models import User
 from property.models import Property
+
 class Offer(models.Model):
     offer_id = models.AutoField(primary_key=True)
     user = models.ForeignKey('user.User', on_delete=models.CASCADE)
